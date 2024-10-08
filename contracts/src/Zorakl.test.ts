2e710f32b1ddf0d4ecdf734dccd8017fd3f2b428
brunolune <brunocoupier@protonmail.com>
2024-10-08 21:47:34 +0200

    selected zorakl.test.ts from feat/getPrice to commit to bruno

--- contracts/src/Zorakl.test.ts
+++ contracts/src/Zorakl.test.ts
@@ -1,6 +1,6 @@
-import { AccountUpdate, Field, Mina, PrivateKey, PublicKey } from 'o1js';
-import { Zorakl, ORACLE_PUBLIC_KEY } from './Zorakl';
-
+import { AccountUpdate, Field, Mina, PrivateKey, PublicKey, Signature } from 'o1js';
+import { Zorakl } from './Zorakl';
+//import { PriceData } from './Zorakl';
 
 /*
  * This file specifies how to test the `Zorakl` example smart contract.
@@ -8,6 +8,9 @@ import { Zorakl, ORACLE_PUBLIC_KEY } from './Zorakl';
 
 
 let proofsEnabled = false;
+// The public key of our trusted data provider
+const ORACLE_PUBLIC_KEY =
+  'B62qpDUv13RzSnUdkesu9m78YGysb6KUuCCAdfxs8NX3oksm6rrBF1d';
 
 describe('Zorakl', () => {
   let deployerAccount: Mina.TestPublicKey,
@@ -44,23 +47,110 @@ describe('Zorakl', () => {
     await txn.sign([deployerKey, zkAppPrivateKey]).send();
   }
 
-  it('generates and deploys the `Zorakl` smart contract', async () => {
+  it('generates and deploys the `OracleExample` smart contract', async () => {
     await localDeploy();
-    const oraclePubKey = zkApp.oraclePublicKey.get();
-    expect(oraclePubKey).toEqual(PublicKey.fromBase58(ORACLE_PUBLIC_KEY));
-  });
-  describe('Fetch Data from Oracles', () => {
-    it.todo('fetches the price data from the oracle with correct signature');
-    it.todo('fetches the price data from the oracle with incorrect signature');
-    it.todo('fetches the price data from the oracle with correct signature and publish token name to trade');
-  });
-  describe('Trade with Price Data', () => {
-    describe('Buy', () => {
-      it.todo('buys the token with the price data from the oracle');
-    });
-    describe('Sell', () => {
-      it.todo('sells the token with the price data from the oracle');
-    });
+    const oraclePublicKey = zkApp.oraclePublicKey.get();
+    expect(oraclePublicKey).toEqual(PublicKey.fromBase58(ORACLE_PUBLIC_KEY));
   });
 
+  describe('hardcoded values', () => {
+    it('emits a `time` and `price` events containing the pricefeed time and price if the provider is valid source', async () => {
+      await localDeploy();
+
+      const price = Field(53008900);
+      const time = Field(1728406858);
+      //const priceData = new PriceData({ price, time });
+      const signature = Signature.fromBase58(
+        '7mXJAj3ewMN6ziouSigZ5x8QECUSb6u54Qgk4pdx65nUTm95t2h8mkthAvoEzA6E1j2V2PAiPQwEnfNonKPHJPrHpX8sZTVf'
+      );
+
+      const txn = await Mina.transaction(senderAccount, async () => {
+        await zkApp.verify(time, price, signature);
+      });
+      await txn.prove();
+      await txn.sign([senderKey]).send();
+
+      const events = await zkApp.fetchEvents();
+      const verifiedEventValuePrice = events[1].event.data.toFields(null)[0];
+      expect(verifiedEventValuePrice).toEqual(price);
+      const verifiedEventValueTime = events[0].event.data.toFields(null)[0];
+      expect(verifiedEventValueTime).toEqual(time);
+    });
+
+    /*it('throws an error if the credit score is below 700 even if the provided signature is valid', async () => {
+      await localDeploy();
+
+      const id = Field(1);
+      const creditScore = Field(536);
+      const signature = Signature.fromBase58(
+        '7mXXnqMx6YodEkySD3yQ5WK7CCqRL1MBRTASNhrm48oR4EPmenD2NjJqWpFNZnityFTZX5mWuHS1WhRnbdxSTPzytuCgMGuL'
+      );
+
+      expect(async () => {
+        const txn = await Mina.transaction(senderAccount, async () => {
+          await zkApp.verify(id, creditScore, signature);
+        });
+      }).rejects;
+    });
+
+    it('throws an error if the credit score is above 700 and the provided signature is invalid', async () => {
+      await localDeploy();
+
+      const id = Field(1);
+      const creditScore = Field(787);
+      const signature = Signature.fromBase58(
+        '7mXPv97hRN7AiUxBjuHgeWjzoSgL3z61a5QZacVgd1PEGain6FmyxQ8pbAYd5oycwLcAbqJLdezY7PRAUVtokFaQP8AJDEGX'
+      );
+
+      expect(async () => {
+        const txn = await Mina.transaction(senderAccount, async () => {
+          await zkApp.verify(id, creditScore, signature);
+        });
+      }).rejects;
+    });*/
+  });
+
+  /*describe('actual API requests', () => {
+    it('emits an `id` event containing the users id if their credit score is above 700 and the provided signature is valid', async () => {
+      await localDeploy();
+
+      const response = await fetch(
+        'https://07-oracles.vercel.app/api/credit-score?user=1'
+      );
+      const data = await response.json();
+
+      const id = Field(data.data.id);
+      const creditScore = Field(data.data.creditScore);
+      const signature = Signature.fromBase58(data.signature);
+
+      const txn = await Mina.transaction(senderAccount, async () => {
+        await zkApp.verify(id, creditScore, signature);
+      });
+      await txn.prove();
+      await txn.sign([senderKey]).send();
+
+      const events = await zkApp.fetchEvents();
+      const verifiedEventValue = events[0].event.data.toFields(null)[0];
+      expect(verifiedEventValue).toEqual(id);
+    });
+
+    it('throws an error if the credit score is below 700 even if the provided signature is valid', async () => {
+      await localDeploy();
+
+      const response = await fetch(
+        'https://07-oracles.vercel.app/api/credit-score?user=2'
+      );
+      const data = await response.json();
+
+      const id = Field(data.data.id);
+      const creditScore = Field(data.data.creditScore);
+      const signature = Signature.fromBase58(data.signature);
+
+      expect(async () => {
+        const txn = await Mina.transaction(senderAccount, async () => {
+          await zkApp.verify(id, creditScore, signature);
+        });
+      }).rejects;
+    });
+  });*/
 });
